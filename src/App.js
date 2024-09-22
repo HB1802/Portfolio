909d140b8db3366e4e3f57a1bae8252cde60d6f3
Harshit Bisht <harshitbisht02@gmail.com>
2024-09-22 12:45:35 +0530

    added initial files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Header from './Components/Header';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+      <Route path="/" exact={true} element={<Home/>}/>
+      </Routes>
+    </BrowserRouter>
+      
+    
+  );
+}
+
+export default App;
